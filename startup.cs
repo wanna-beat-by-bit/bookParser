18039ebddc5086da7d5922c35796e08bc4d529ce
Alex Fomin <alexkillzone53@gmail.com>
2023-01-26 05:10:58 +1000

    Исправлена инъекция БД и бизнес логики

--- startup.cs
+++ startup.cs
@@ -3,21 +3,42 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+using bookParser.Models;
+using bookParser.repository;
+using bookParser.Logic;
 
 namespace bookParser.startup{
     public class Startup
     {
+        public IConfiguration Configuration {get;}
+        public Startup(IConfiguration configuration){
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton<IConfiguration>(Configuration);
+
+            var connectionString = "server=localhost;port=5436;userid=postgres;database=postgres;password=qwerty";
+
+            var builder = new NpgsqlConnectionStringBuilder(connectionString);
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(builder.ConnectionString));
+            services.AddScoped<IRepository>(_ => new Repository(builder.ConnectionString));
+            services.AddScoped<IBLogic, BLogic>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
             app.UseRouting();
 
