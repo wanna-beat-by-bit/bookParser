cdf2e1ad4ce6f8ed92dacca4e29f8bb4005ab34b
Alex Fomin <alexkillzone53@gmail.com>
2023-01-26 05:15:10 +1000

    Контроллер работоспособен

--- Controller/controller.cs
+++ Controller/controller.cs
@@ -1,33 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Npgsql;
 
 using bookParser.repository;
+using bookParser.Logic;
 
 namespace bookParser.Controllers{
     [ApiController]
     [Route("api/testing")]
-    public class TestController : ControllerBase, IHandlerLogic
+    public class TestController : ControllerBase
     {
-        private IRepository _dbRepo;
-
-        public TestController(IRepository dbRepo){
+        private readonly IRepository _dbRepo;
+        private readonly IBLogic _bLogic;
+        private static object? aWord;
+        public TestController(IRepository dbRepo, IBLogic bLogic){
             _dbRepo = dbRepo;
+            _bLogic = bLogic;
         }
         
-        [HttpPost]
-        public IActionResult Post([FromBody] object? data)
+        [HttpPost("addToDomains")]
+        public IActionResult addToDomains([FromBody] object data)
         {
-            //var testData = new { message = "Hello from post" };
-            Console.WriteLine("HEELOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-            return Ok(data);
+            aWord = data; 
+            return Ok(new {Status = aWord});
         }
 
-        [HttpGet("lol")]
-        public IActionResult GetSome(){
-            var testData = new[]{
-                new {id = 1, name = "jeff"}
-            };
-            return Ok(new {testData});
+        [HttpPost("readBooks")]
+        public IActionResult readBooks([FromBody] object data)
+        {
+            aWord = data; 
+            return Ok(new {Status = aWord});
         }
+
+        [HttpGet("getAllBooks")]
+        public IActionResult getAllBooks(){
+
+            return Ok(new {content = aWord});
+        }
+
+        [HttpGet("logicTest")]
+        public IActionResult logicTest(){
+            _bLogic.logicTest();
+            return Ok();
+        }
+
     }
 }
